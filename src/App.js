5e124ab5b13060a2e79102885c08d49ffe055b69
Matej <matej.mijok@gmail.com>
2023-11-19 15:18:47 +0100

    Update App.js and App.css

--- src/App.js
+++ src/App.js
@@ -16,28 +16,44 @@ function App() {
     setActiveTabIndex(event.target.activeTabIndex);
   }
 
-  
   return (
-    <div className='container-fluid' id='container'>
-      <div className='container-fluid'>
-        <ul className='navbar-nav'>
+    <div class='container-fluid' id='container'>
+      <div class='container-fluid'>
+        <ul class='navbar-nav'>
           <md-tabs id='tabs' ref={myRef}>
             <md-secondary-tab class='tabs' id='home-tab' aria-controls='menu-panel' active>Home</md-secondary-tab>
             <md-secondary-tab class='tabs' id='play-tab' aria-controls='play-panel'>Play</md-secondary-tab>
-            <md-secondary-tab class='tabs' id='stats-tab' aria-controls='stats-panel'>Stats</md-secondary-tab>
+            <md-secondary-tab class='tabs' id='stats-tab' aria-controls='stats-panel'>Statistics</md-secondary-tab>
           </md-tabs>
+        </ul>
+      </div>
 
-          <div id='home-panel' role='tabpanel' aria-label='home-tab' hidden={activeTabIndex !== 0}>
-            <h1>Welcome!</h1>
+          <div class='container-fluid m-3' id='home-panel' role='tabpanel' aria-label='home-tab' hidden={activeTabIndex !== 0}>
+
           </div>
           <div id='play-panel' role='tabpanel' aria-label='play-tab' hidden={activeTabIndex !== 1}>
             <p>proba 2</p>
           </div>
-          <div id='stats-panel' role='tabpanel' aria-label='stats-tab' hidden={activeTabIndex !== 2}>
-            <p>proba 3</p>
+
+          <div id='stats-panel' class='container-fluid' role='tabpanel' aria-label='stats-tab' hidden={activeTabIndex !== 2}>
+            <div class='jumbotron jumbotron-fluid'>
+                <h1 class="display-4 text-center mt-3" id='text'>Welcome!</h1>
+                <p class='lead text-center' id='text'>We appreaciate your enthusiasm but you need to log in or register first!</p>
+                <p class='lead text-center' id='text'>You can log in by clicking the appropriate button below and filling in the form!</p>
+
+                <div class='container-fluid'>
+                  <div class='row justify-content-md-center'>
+                    <div class='col-md-auto text-center mt-3'>
+                      <md-filled-tonal-button id='button'>Log in</md-filled-tonal-button>
+                    </div>
+                    
+                    <div class='col-md-auto text-center mt-3'>
+                      <md-filled-tonal-button id='button'>Register</md-filled-tonal-button>
+                    </div>
+                  </div>
+                </div>
+            </div>
           </div>
-        </ul>
-      </div>
     </div>
   );
 }
