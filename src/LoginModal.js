0d3c3a7b3137c561e84c5c5437861dcd01ce6411
Matej <matej.mijok@gmail.com>
2023-11-20 18:52:11 +0100

    Update LoginModal.js

--- src/LoginModal.js
+++ src/LoginModal.js
@@ -10,17 +10,24 @@ function LoginModal({ show, handleClose }) {
     <div className='container-fluid align-items-center justify-content-center' id='modalStyle'>
       <Modal show={show} onHide={handleClose} id='modal-header  '>
         <Modal.Header id='modalStyle'>
-          <Modal.Title id='text'>Log in</Modal.Title>
+            <Modal.Title id='text'>Log in</Modal.Title>
         </Modal.Header>
+        <form>
         <Modal.Body id='modalStyle'>
-          <md-filled-text-field label='E-mail' type='email' id='textField'></md-filled-text-field>
-          <md-filled-text-field label='Password' type='password' id='textField'></md-filled-text-field>
+            <div className='container-fluid d-flex align-items-center justify-content-center'>
+                <div className='d-flex flex-column'>
+                <md-filled-text-field label='Username' type='text' id='textField'></md-filled-text-field>
+                    <md-filled-text-field label='E-mail' type='email' id='textField' class='mt-2'></md-filled-text-field>
+                    <md-filled-text-field label='Password' type='password' id='textField' class='mt-2'></md-filled-text-field>
+                </div>
+            </div>
         </Modal.Body>
 
         <Modal.Footer id='modalStyle'>
-          <md-filled-tonal-button id='secondaryTonalButton' onClick={handleClose}>Cancel</md-filled-tonal-button>
-          <md-filled-button id='primaryButton' type='submit'>Log in</md-filled-button>
+            <md-filled-tonal-button id='secondaryTonalButton' onClick={handleClose}>Cancel</md-filled-tonal-button>
+            <md-filled-button id='primaryButton' type='submit'>Log in</md-filled-button>
         </Modal.Footer>
+        </form>
       </Modal>
     </div>
     </>
