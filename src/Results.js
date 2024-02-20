0b0cba9938e94cbeab936902eab1e662c2201ad8
Matej <matej.mijok@gmail.com>
2024-02-20 16:19:53 +0100

    Implement results page and fix display issue when page is scrollable

--- src/Results.js
+++ src/Results.js
@@ -8,15 +8,35 @@ import { useNavigate } from 'react-router-dom';
 function Results() {
   const sessionData = JSON.parse(localStorage.getItem('sessionData'));
   const lastQuiz = JSON.parse(localStorage.getItem('lastQuiz'));
+  
+  const totalQuestions = lastQuiz ? lastQuiz.answers.length : 0;
+  let correctAnswers = 0;
+  
+  if (lastQuiz) {
+    lastQuiz.questions.forEach((question, index) => {
+      if (lastQuiz.answers[index] === question.correctAnswer) {
+        correctAnswers++;
+      }
+    });
+  }
 
   return (
     <>
-      <div className='container-fluid text-center'>
-          <p className='display-4 text-center mt-5' id='text' >TO DO</p>
-          <p className='display-4 text-center mt-5' id='text' >Correct answers: {sessionData.correctAnswers}</p>
-          <p className='display-4 text-center mt-5' id='text' >Games played: {sessionData.gamesPlayed}</p>
-          <p className='display-4 text-center mt-5' id='text' >Questions answered: {sessionData.questionsAnswered}</p>
+      <div className='container-fluid text-center mt-5'>
+        <h1 id="text">Quiz Results</h1>
+        <h4 className='text-center mt-2' id='text' >Total questions: {totalQuestions}</h4>
+        <h4 className='text-center mt-2' id='text' >Correct answers: {correctAnswers}/{totalQuestions}</h4>
       </div>
+      <div className="container-fluid text-center mt-5">
+        <h2 id="text">Answer Sheet</h2>
+          {lastQuiz && lastQuiz.questions.map((question, index) => (
+            <div key={index} className="question">
+              <h3 id="text" className='text-center mt-3'>{index + 1}. {question.questionText}</h3>
+              <h4 id="text" className='text-center mt-3'>Correct Answer: {question.correctAnswer}</h4>
+              <h4 id="text" className='text-center mt-3'>Your Answer: {lastQuiz.answers[index]}</h4>
+            </div>
+          ))}
+        </div>
     </>
   );
 }
